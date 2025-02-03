21a1e0e39b8ebd128934f113960d0c86c01bfd6f
Johnkat Mj <johnkatembue4@gmail.com>
2025-02-03 12:35:51 +0200

    feat: improve toggleNavbar utility

--- packages/utilities/src/toggler/toggler.ts
+++ packages/utilities/src/toggler/toggler.ts
@@ -31,32 +31,36 @@ export const actionToggler = (options: TogglerOptions) => {
 
 
 
-export const toggleNavbar = ({ navbarElement, allowBoyScroll = false }: { navbarElement: string | HTMLElement, allowBoyScroll?: boolean }) => {
+export const toggleNavbar = ({ navbarElement, allowBoyScroll = false, onToggle }: { navbarElement: string | HTMLElement, allowBoyScroll?: boolean, onToggle?: ({ isExpanded }: { isExpanded: boolean }) => void }) => {
 	const navbar = typeof navbarElement === "string" ? $(navbarElement) as HTMLElement : navbarElement;
 	if (!(navbar instanceof HTMLElement)) return
 
 	const id = navbar.getAttribute("id")
 	const trigger = $(`[data-nav-trigger][data-toggle-nav=${id}]`);
-	const overlayEl = $(`[data-navbar-id=${id}]`)
+	const overlayEl = $(`[data-nav-overlay][data-navbar-id=${id}]`)
 
 	if (trigger instanceof HTMLButtonElement) {
-		trigger.addEventListener("click", () => {
+		const toggleState = () => {
 			const state = navbar.dataset.state || "close";
-			navbar.setAttribute("data-state", `${state === "open" ? "close" : "open"}`);
-			trigger.setAttribute("data-expanded", `${state === "open" ? "false" : "true"}`);
+			const dataState = state === "open" ? "close" : "open"
+			navbar.setAttribute("data-state", dataState);
+			trigger.setAttribute("aria-expanded", `${state === "open" ? "false" : "true"}`);
 			if (!allowBoyScroll) document.body.style.overflowY = `${state === "open" ? "auto" : "hidden"}`;
 			if (overlayEl) {
 				overlayEl.ariaHidden = "true"
 				overlayEl.setAttribute("data-state", "open")
 			}
-		});
+			onToggle?.({ isExpanded: dataState === "open" })
+		}
+		trigger.addEventListener("click", toggleState);
 		const closeNavbar = () => {
 			navbar.setAttribute("data-state", "close");
-			trigger.setAttribute("data-expanded", "false");
+			trigger.setAttribute("aria-expanded", "false");
 			if (!allowBoyScroll) document.body.style.overflowY = "auto";
 			if (overlayEl) {
 				overlayEl.setAttribute("data-state", "close")
 			}
+			onToggle?.({ isExpanded: false })
 		}
 		navbar.addEventListener("click", closeNavbar);
 		if (overlayEl instanceof HTMLElement && !overlayEl.hasAttribute("data-static-overlay")) {
