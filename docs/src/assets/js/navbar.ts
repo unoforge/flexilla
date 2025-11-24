851eb720712f9ec22be2e4b9bf1e15ae2e8400cc
Johnkat Mj <johnkatembue4@gmail.com>
2025-11-24 11:03:18 +0200

    chore: init new documentation site

--- docs/src/assets/js/navbar.ts
+++
@@ -1,5 +0,0 @@
-import { toggleNavbar } from "@flexilla/utilities"
-
-export const initNavbar = () => {
-  toggleNavbar({ navbarElement: "[data-app-navbar]" })
-}
--- docs/src/const/index.ts
+++
@@ -1,5 +0,0 @@
-export const side_domain = "flexilla-docs.vercel.app"
-export const siteUrl = import.meta.env.SITE
-export const keywordsDef = "Flexilla, interactive components, headless library, CSS-agnostic, UI Library, UnoCSS Components, TailwindCSS Components, Laravel Components, web development "
-export const defaultOgImage = "/flexilla-cover.png"
-export const defaultOgAlt = "Flexilla: Versatile Interactive Component Library"
